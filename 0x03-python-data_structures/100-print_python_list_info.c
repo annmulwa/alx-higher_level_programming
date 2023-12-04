b1a812e31212afe0b7630ebb6444ee73424ba629
annmulwa <annmulwa2000@gmail.com>
2023-12-04 03:42:34 -0800

    python list

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,22 @@
+#include <python.h>
+
+/**
+ * print_python_list_info - prints information about python lists
+ * @p: PyObject list
+ */
+void print_python_list_info(PyObject *p)
+{
+	int j;
+	PyObject *py;
+
+	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+
+	for (j = 0; j < Py_SIZE(p); j++)
+	{
+		printf("Element %d: ", j);
+		
+		py = PyList_GetItem(p, j);
+		printf("%s\n", Py_TYPE(py)->tp_name);
+	}
+}
