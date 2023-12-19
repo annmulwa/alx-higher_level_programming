8044672805f70ffd6b063f67da008d3c100c8b0d
annmulwa <annmulwa2000@gmail.com>
2023-12-19 05:55:04 -0800

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''square module'''
+
+
+class Square:
+    '''defines a square'''
+    def __init__(self, size):
+        '''size is a private instance attribute'''
+        self.__size = size
