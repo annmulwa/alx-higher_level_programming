32d0b517c86cde56c4d4ac4bd5b419df68884fdd
annmulwa <annmulwa2000@gmail.com>
2024-01-08 06:32:52 -0800

    inheritance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+Defines a class MyInt that inherits from int.
+'''
+
+
+class MyInt(int):
+    '''
+    Inverts int operators == and !=
+    '''
+
+    def __eq__(self, value):
+        '''
+        inverts ==
+        '''
+        return self.real != value
+
+    def __ne__(self, value):
+        '''
+        inverts !=
+        '''
+        return self.real == value
