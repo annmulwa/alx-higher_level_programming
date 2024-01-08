e05cbb50eac941dafe6d12b4497d7ef782ffbf30
annmulwa <annmulwa2000@gmail.com>
2024-01-08 05:06:43 -0800

    inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+contains an empty class
+'''
+
+
+class BaseGeometry:
+    '''
+    BaseGeometry class
+    '''
+    def area(self):
+        '''
+        represents an area
+        '''
+        raise Exception("area() is not implemented")
