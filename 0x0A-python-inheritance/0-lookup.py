21250be7bf5949be9ed2badaab93c85186e5c6c2
annmulwa <annmulwa2000@gmail.com>
2024-01-08 03:59:50 -0800

    inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 '''
-defines a function that returns a list of attributes 
+defines a function that returns a list of attributes
 and available methods of an object
 '''
+
+
 def lookup(obj):
     '''
     returns a list of available attributes
