852158192fda7647242fde33735f74b45ab1cb76
annmulwa <annmulwa2000@gmail.com>
2024-01-09 06:11:09 -0800

    file I/O

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+defines a class atudent
+'''
+
+
+class Student:
+    '''
+    Represents a student
+    '''
+    def __init__(self, first_name, last_name, age):
+        '''
+        Initializes a new student with
+        public instance attributes
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+        retrieves a dictionary representation of a
+        student
+        '''
+        if (type(attrs) == list and
+                all(type(elem) == str for elem in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
