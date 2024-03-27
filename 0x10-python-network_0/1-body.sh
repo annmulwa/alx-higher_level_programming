8d893ad27d28b9d11c9ab0e713f527d1dc929371
annmulwa <annmulwa2000@gmail.com>
2024-03-27 07:26:43 -0700

    network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display only body of a 200 status code response
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1"); [ "$response" -eq 200 ] && curl -s "$1"
+curl -s -w "%{http_code}" "$1" | awk 'NR==1 && $0 == 200 {p=1} p' | tail -n +2
