683dd792cd77cdebb094ba178b6c4e04c7ea70ff
annmulwa <annmulwa2000@gmail.com>
2024-03-28 05:40:52 -0700

    python network

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,6 +11,10 @@ if __name__ == "__main__":
     url = "https://api.github.com/{}/{}/commits".format(argv[2], argv[1])
     response = requests.get(url)
     commits = response.json()
-    for commit in commits[:10]:
-        print(commit.get('sha'), end=": ")
-        print(commit.get('commit').get('author').get('name'))
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
