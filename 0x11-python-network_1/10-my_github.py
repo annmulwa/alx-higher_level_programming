7945c58f34fb1d767d5e6e65a0cdd4dfde4a0781
annmulwa <annmulwa2000@gmail.com>
2024-03-28 05:17:04 -0700

    python network

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,11 +6,11 @@ and password) and uses the GitHub API to display your id
 
 
 import sys
-from requests.auth import HTTPBasicAuth
 import requests
+from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    url = "https://api.github.com/users/{}".format(sys.argv[1])
+    url = "https://api.github.com/user"
     response = requests.get(url,
                             auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
     print(response.json().get('id'))
