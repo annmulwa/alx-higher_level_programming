f2b22787105f173463c36f9e48d7cc86820a54e2
annmulwa <annmulwa2000@gmail.com>
2024-03-28 02:59:55 -0700

    python network

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,7 +12,7 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"Your email is": sys.argv[2]}
+    value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
 
     req = urllib.request.Request(url, data)
