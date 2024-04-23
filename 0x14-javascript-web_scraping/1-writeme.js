8bc7a4183829f3076166a1329fc8d541b94348a6
annmulwa <annmulwa2000@gmail.com>
2024-04-23 04:05:32 -0700

    javascript

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,5 @@ const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (error) {
   if (error) {
     console.error('ERROR:', error);
-  } else {
-    console.log('FILE WRITTEN SUCCESSFULLY');
   }
 });
